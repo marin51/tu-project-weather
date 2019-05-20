0c2767fcd62cdceceb2fa503664f31b86cb76453
Marin <marin.marinov@succevo.com>
2019-05-20 08:41:53 +0300

    restructure

--- www/js/startapp.js
+++ www/js/startapp.js
@@ -27,7 +27,13 @@ var startApp = (function() {
                 if (btnIndex === 1) {
                     city = 'Sofia';
                     $('.progess-icon').addClass('hide');
-                    weather.showDailyForest(city);
+                    weather.getDailyForecast(city).then(function(results) {
+                        saveDailyForecastCitiesLocally(results);
+                        showWeatherData(results);
+                    }, function(error) {
+                        $('#weather-data').hide();
+                        $('.home-message').text("Error retrieving data. ");
+                    });
                     console.log('save');
                 } else {
                     ons.notification.prompt({
@@ -38,9 +44,15 @@ var startApp = (function() {
                         if (typedCity) {
                             city = typedCity;
 
-                            weather.showDailyForest(typedCity);
+                            weather.getDailyForecast(typedCity).then(function(results) {
+                                saveDailyForecastCitiesLocally(results);
+                                showWeatherData(results);
+                            }, function(error) {
+                                $('#weather-data').hide();
+                                $('.home-message').text("Error retrieving data. ");
+                            });
                             $('.progess-icon').addClass('hide');
-                            console.log(city);
+
                         } else {
                             $('.progess-icon').addClass('hide');
                             $('.fail-icon').removeClass('hide');
@@ -55,13 +67,58 @@ var startApp = (function() {
             for (const key in json_cities) {
                 if (json_cities.hasOwnProperty(key)) {
                     const element = json_cities[key];
-                    var buttonHTML = '<button class="button--cta" onclick="weather.showDailyForest(\'' + json_cities[key].name + '\')">See forecast for ' + json_cities[key].name + '</button>';
+                    var buttonHTML = '<button class="button--cta" onclick="weather.getDailyForecast(\'' + json_cities[key].name + '\')">See forecast for ' + json_cities[key].name + '</button>';
                     $('#home-page-template .main-container').append(buttonHTML);
                 }
             }
         }
 
 
+    }
+
+
+
+
+    function showWeatherData(results) {
+        var html = '';
+        if ($('.main-container #weather-data').length) {
+            $('.main-container #weather-data').html('');
+        } else {
+            $('#home-page-template .main-container').append(ons.createElement('<ons-list id="weather-data"></ons-list>'))
+        }
+
+        if (results.weather.length) {
+            var sunriseDate = new Date(results.sys.sunrise * 1000),
+                sunsetDate = new Date(results.sys.sunset * 1000);
+
+            html += '<ons-list-header id="title">City ' + results.name + ' <img class="done-icon hide" src="../www/res/baseline-done-24px.svg" alt="" srcset=""></ons-list-header>';
+            html += '<ons-list-item id="temperature">Temperature  ' + results.main.temp + '°C</ons-list-item>';
+            html += '<ons-list-item id="min-temp">Today maximum :' + results.main.temp_min + '°C</ons-list-item>';
+            html += '<ons-list-item id="max-tem">Today minimum : ' + results.main.temp_max + '°C</ons-list-item>';
+            html += '<ons-list-item id="wind-speed">Wind speed: ' + results.wind.speed + ' km/h</ons-list-item>';
+            html += '<ons-list-item id="humidity">Humidity: ' + results.main.humidity + ' %</ons-list-item>';
+            html += '<ons-list-item id="visibility">Visibility: ' + results.weather[0].main + '</ons-list-item>';
+
+            html += '<ons-list-item id="sunrise">Sunrise: ' + sunriseDate.toLocaleTimeString() + '</ons-list-item>';
+
+            html += '<ons-list-item id="sunset">Sunset: ' + sunsetDate.toLocaleTimeString() + '</ons-list-item>';
+            html += '<button onclick="startApp.reset()" class="button button--outline footer-button left">Reset</button>';
+            html += '<button onclick="weeklyforecast.init(\'' + results.name + '\')" class="button button--outline footer-button right">5 days forecast</button>';
+
+
+
+            $('#home-page-template .main-container #weather-data').append(html);
+            $('.home-message').hide();
+            $('#check-location-button').hide();
+
+            $('#home-reset-button').show();
+            $('#weather-data').show();
+
+        } else {
+            $('#weather-data').hide();
+            $('.home-message').text("Error retrieving data. ");
+            ons.notification.alert('Ooops! It seems there are an error. Please try again later.');
+        }
     }
 
     function reset() {
--- www/js/weather.js
+++ www/js/weather.js
@@ -1,11 +1,12 @@
 var weather = (function() {
     var OpenWeatherAppKey = "271e0eab66001b4207410ec33d7cf4f9";
 
-    function showDailyForest(city) {
+    function getDailyForecast(city) {
         alert('city ' + city)
-            // if (json_cities.hasOwnProperty(city) && isActualForecast(json_cities[city].date)) {
-            // showWeatherData(json_cities[city]);
-            // } else {
+        var dfd = jQuery.Deferred();
+        // if (json_cities.hasOwnProperty(city) && isActualForecast(json_cities[city].date)) {
+        // showWeatherData(json_cities[city]);
+        // } else {
         var link = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + ',BG&appid=' + OpenWeatherAppKey + '&units=metric';
         alert(link)
         $.ajax({
@@ -14,16 +15,15 @@ var weather = (function() {
 
             success: function(results) {
                 alert(JSON.stringify(results))
-                saveDailyForecastCitiesLocally(results);
-                showWeatherData(results);
-                // dfd.resolve('success');
+                dfd.resolve(results);
             },
             error: function(error) {
                 alert(JSON.stringify(error))
-                    // dfd.reject(error);
+                dfd.reject(error);
             }
         });
         // }
+        return dfd.promise();
     }
 
     function getWeeklyForecast(city) {
@@ -49,51 +49,8 @@ var weather = (function() {
         }
         return dfd.promise();
     }
-
-    function showWeatherData(results) {
-        var html = '';
-        if ($('.main-container #weather-data').length) {
-            $('.main-container #weather-data').html('');
-        } else {
-            $('#home-page-template .main-container').append(ons.createElement('<ons-list id="weather-data"></ons-list>'))
-        }
-
-        if (results.weather.length) {
-            var sunriseDate = new Date(results.sys.sunrise * 1000),
-                sunsetDate = new Date(results.sys.sunset * 1000);
-
-            html += '<ons-list-header id="title">City ' + results.name + ' <img class="done-icon hide" src="../www/res/baseline-done-24px.svg" alt="" srcset=""></ons-list-header>';
-            html += '<ons-list-item id="temperature">Temperature  ' + results.main.temp + '°C</ons-list-item>';
-            html += '<ons-list-item id="min-temp">Today maximum :' + results.main.temp_min + '°C</ons-list-item>';
-            html += '<ons-list-item id="max-tem">Today minimum : ' + results.main.temp_max + '°C</ons-list-item>';
-            html += '<ons-list-item id="wind-speed">Wind speed: ' + results.wind.speed + ' km/h</ons-list-item>';
-            html += '<ons-list-item id="humidity">Humidity: ' + results.main.humidity + ' %</ons-list-item>';
-            html += '<ons-list-item id="visibility">Visibility: ' + results.weather[0].main + '</ons-list-item>';
-
-            html += '<ons-list-item id="sunrise">Sunrise: ' + sunriseDate.toLocaleTimeString() + '</ons-list-item>';
-
-            html += '<ons-list-item id="sunset">Sunset: ' + sunsetDate.toLocaleTimeString() + '</ons-list-item>';
-            html += '<button onclick="startApp.reset()" class="button button--outline footer-button left">Reset</button>';
-            html += '<button onclick="weeklyforecast.init(\'' + results.name + '\')" class="button button--outline footer-button right">5 days forecast</button>';
-
-
-
-            $('#home-page-template .main-container #weather-data').append(html);
-            $('.home-message').hide();
-            $('#check-location-button').hide();
-
-            $('#home-reset-button').show();
-            $('#weather-data').show();
-
-        } else {
-            $('#weather-data').hide();
-            $('.home-message').text("Error retrieving data. ");
-            ons.notification.alert('Ooops! It seems there are an error. Please try again later.');
-        }
-    }
-
     return {
-        showDailyForest: showDailyForest,
+        getDailyForecast: getDailyForecast,
         getWeeklyForecast: getWeeklyForecast
     }
 })();
